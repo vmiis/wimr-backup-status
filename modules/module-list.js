ec10277fed843f2a3ef79bc94b095e411deb3826
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-11-04 12:46:19 +1100

    update

---
+++ modules/module-list.js
@@ -0,0 +1,11 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var p=""; //put a unique prefix to avoid conflict with others 
+    var H=$vm.hosting_path+"/modules";
+    var m=$vm.module_list;
+    var api="wimr";
+    //-------------------------------------------------------------------------------------
+    m[p+"cms-and-research-database"]     ={url:H+"/business/cms-and-research-database/page.html",prefix:p,router:1};
+    m[p+"sleepwareg3-adult-database"]={url:H+"/business/sleepwareg3-adult-database/page.html",prefix:p,router:1};
+    m[p+"sleepwareg3-pediatrics-database"]={url:H+"/business/sleepwareg3-pediatrics-database/page.html",prefix:p,router:1};
+})();
